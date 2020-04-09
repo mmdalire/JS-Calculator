c5b07a74dbbf5f133c95b51b433f664ec2dae7ee
Mark Dalire <mmdalire@gmail.com>
2020-04-09 18:19:22 +0800

    Final version of the calculator 1) Fixes the problem with the equals button. 2) Positive and negative button is operational 3) Numbers with fractional part can be computated

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const numbers = document.querySelectorAll('[data-number]');
 const operations = document.querySelectorAll('[data-operation]');
-//const equals = document.querySelector('[data-equals');
+const equals = document.querySelector('[data-equals');
+const positiveNegative = document.querySelector('[data-positive-negative');
 const clear = document.querySelector('[data-clear]');
 const clearAll = document.querySelector('[data-clear-all]');
 let display = document.querySelector('.calculator-container__display');
@@ -8,7 +9,6 @@ const maximumDigits = 10;
 
 let previousNumber, currentNumber;
 let previousOperator ,currentOperator;
-let equalPressed = false;
 
 const appendNumbers = number => {
     //When the calculator is starting up
@@ -18,25 +18,27 @@ const appendNumbers = number => {
             currentNumber = '0.';
             return;
         }
+        //When initially the number is 0 and the user clicks 0 again
+        if(display.textContent === '0' && number === '0') return;
         //When a number is clicked first
         currentNumber = number;
         return;
     }
     //When a number in currentNumber exists
-    currentNumber += number;
+    currentNumber += number
 }
 
 const placeNumbers = number => {
     let numDigit = number.textContent;
-    if(display.textContent.length >= maximumDigits) return;  //When the calculator reaches its maximum digit display
+    if(display.textContent.length > maximumDigits) return;  //When the calculator reaches its maximum digit display
     if(numDigit === '.' && display.textContent.includes('.')) return; //Avoiding placing of two or more decimal places
     appendNumbers(numDigit);
 }
 
 const clearDigit = () => {
     if(currentNumber === undefined) return;
-    
-    currentNumber = currentNumber.toString().slice(0, -1);
+    currentNumber = currentNumber.toString().slice(0, -1); //Remove the last digit
+    //If it reaches an empty string, clears out the display
     if(currentNumber === '') {
         currentNumber = undefined;
         display.textContent = '0';
@@ -46,8 +48,16 @@ const clearDigit = () => {
 const clearAllDigits = () => {
     previousNumber = undefined;
     currentNumber = undefined;
-    previousSign = undefined;
+    previousOperator = undefined;
+    currentOperator = undefined;
     display.textContent = '0';
+}
+
+const changeSign = () => {
+    if(currentNumber !== undefined) {
+        currentNumber *= -1;
+    }
+    return;
 }
 
 const chooseOperation = operationSign => {
@@ -58,17 +68,15 @@ const chooseOperation = operationSign => {
         currentNumber = undefined;
         return;
     } 
-    else {
-        currentOperator = operationSign;
-        calculation();
-        return;
-    }
+    currentOperator = operationSign;
+    calculation();
+    return;
 }
 
 const calculation = () => {
     let calculatedValue;
 
-    switch(previousOperator || currentOperator) {
+    switch(previousOperator) {
         case '+':
             calculatedValue = parseFloat(previousNumber) + parseFloat(currentNumber);
             break;
@@ -84,6 +92,18 @@ const calculation = () => {
         default:
             return;
     }
+
+    //When the equal button is pressed, it shows the final value and resets the display
+    if(currentOperator === '=') {
+        currentNumber = calculatedValue;
+        updateDisplay();
+        previousNumber = undefined;
+        currentNumber = undefined;
+        previousOperator = undefined;
+        currentOperator = undefined;
+        return;
+    }
+
     previousOperator = currentOperator;
     previousNumber = calculatedValue;
     currentNumber = calculatedValue;
@@ -92,7 +112,6 @@ const calculation = () => {
 }
 
 const updateDisplay = () => {
-    if(display.textContent.length >= maximumDigits) return;
     if(currentNumber === undefined) return;
     display.textContent = currentNumber.toString(); //Displays the current number into the calculator
 }
@@ -115,15 +134,23 @@ clear.addEventListener('click', e => {
 clearAll.addEventListener('click', e => {
     clearAllDigits();
     updateDisplay();
-})
+});
 
+//Change the sign from positive to negative and vice versa
+positiveNegative.addEventListener('click', e => {
+    changeSign();
+    updateDisplay();
+});
+
+//Choosing operations
 operations.forEach(operationBtn => {
     operationBtn.addEventListener('click', e => {
         chooseOperation(operationBtn.textContent);
     });
 });
 
-/*equals.addEventListener('click', e => {
-    equalPressed = true;
+//Equals is pressed
+equals.addEventListener('click', e => {
+    currentOperator = equals.textContent;
     calculation();
-});*/
+});
